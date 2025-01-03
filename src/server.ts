4b26ddb1079e47e571625927358b695cc2dedb5e
idev3105 <khanhcongpruj123@gmail.com>
2025-01-04 01:12:14 +0700

    🗑️ Remove Redis connection before creating socket server

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,12 @@ import 'dotenv/config';
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import { createClient } from 'redis';
 import { createSocketServer } from './socket';
 
 const app = express();
 app.use(cors({ origin: '*' })); // Allow all domains for all routes
 
 const server = http.createServer(app);
-
-// Create redis pub/sub adapter for socket io
-const pubClient = createClient({ url: 'redis://localhost:6379' });
-const subClient = pubClient.duplicate();
-
-await Promise.all([pubClient.connect(), subClient.connect()]);
 
 await createSocketServer(server);
 
